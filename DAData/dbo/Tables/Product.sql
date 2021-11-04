64105f3affd6546343c4519ab45f236cbd59d50a
miravaculka <miravaculka@gmail.com>
2021-11-04 12:49:42 +0100

    Basic tables
    Basic tables- Product and User

---
+++ DAData/dbo/Tables/Product.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+    [Id] INT NOT NULL PRIMARY KEY,
+	[Designation] NVARCHAR(50) NOT NULL, 
+    [Name] NVARCHAR(256) NULL, 
+    [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate()
+    
+)

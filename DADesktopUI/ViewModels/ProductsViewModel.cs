ad52ae8d8782fef9fc5fae1c5a01161058b70407
miravaculka <miravaculka@gmail.com>
2021-11-19 17:22:32 +0100

    Selected Product binded to controls used for Copy new product

--- DADesktopUI/ViewModels/ProductsViewModel.cs
+++ DADesktopUI/ViewModels/ProductsViewModel.cs
@@ -14,6 +14,7 @@ namespace DADesktopUI.ViewModels
     {
         private BindableCollection<ProductModel> _products;
         private IProductEndpoint _productEndpoint;
+        private ICollectionView _fooView;
         public ProductsViewModel(IProductEndpoint productEndpoint)
         {
             _productEndpoint = productEndpoint;
@@ -37,6 +38,27 @@ namespace DADesktopUI.ViewModels
                 NotifyOfPropertyChange(() => Products);
             }
         }
+        private ProductModel _selectedProduct;
 
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            {
+                _selectedProduct = value;
+                SetCopyProductControls();
+            }
+        }
+        public string ProductName { get; set; }
+        public string ProductType { get; set; }
+
+        private void SetCopyProductControls()
+
+        {
+            ProductName = _selectedProduct.Name;
+            NotifyOfPropertyChange(() => ProductName);
+            ProductType = _selectedProduct.Type;
+            NotifyOfPropertyChange(() => ProductType);
+        }
     }
 }

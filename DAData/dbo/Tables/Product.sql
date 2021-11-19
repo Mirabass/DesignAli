9853f7d662f31d498cd90cbbcc86bf0b4b0b2933
miravaculka <miravaculka@gmail.com>
2021-11-19 15:04:11 +0100

    Table of Product extended of some atributes

--- DAData/dbo/Tables/Product.sql
+++ DAData/dbo/Tables/Product.sql
@@ -2,7 +2,9 @@
 (
     [Id] INT NOT NULL PRIMARY KEY,
 	[Designation] NVARCHAR(50) NOT NULL, 
-    [Name] NVARCHAR(256) NULL, 
+    [EAN] BIGINT NOT NULL, 
+    [Name] NVARCHAR(256) NOT NULL, 
+    [Type] NVARCHAR(256) NOT NULL, 
     [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
     [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate()
     

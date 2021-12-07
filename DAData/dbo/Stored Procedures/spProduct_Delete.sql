ca4d8948b60ad52cbf6c02ee54196d9e39981d65
miravaculka <miravaculka@gmail.com>
2021-12-07 15:57:14 +0100

    Delete product now deletes also ColorDesign and Strap

--- DAData/dbo/Stored Procedures/spProduct_Delete.sql
+++ DAData/dbo/Stored Procedures/spProduct_Delete.sql
@@ -3,6 +3,22 @@
 AS
 begin
 	set nocount on;
+
+	declare @StrapId as int, @ColorDesignId as int
+	set @StrapId = (
+			select p.ProductColorDesignId
+			from dbo.Product p
+			where Id = @Id
+		);
+	set @ColorDesignId = (
+		select p.ProductColorDesignId
+			from dbo.Product p
+			where Id = @Id
+		);
 	delete from dbo.Product
 	where Id = @Id
+	delete from dbo.ProductColorDesign
+	where Id = @ColorDesignId
+	delete from dbo.ProductStrap
+	where Id = @StrapId
 end

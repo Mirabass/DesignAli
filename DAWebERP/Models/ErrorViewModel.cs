701fd6c687643da640e9b1baecbe7e2344399c85
miravaculka <miravaculka@gmail.com>
2021-12-15 15:56:33 +0100

    Web app ERP Design Ali PROJECT created

---
+++ DAWebERP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DAWebERP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

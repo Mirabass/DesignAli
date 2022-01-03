8aa78a1b81450731baa48ed739447f4c9bf5480b
miravaculka <miravaculka@gmail.com>
2022-01-03 10:56:22 +0100

    Server-side validation for product create

--- DAWebERP1/Controllers/ProductController.cs
+++ DAWebERP1/Controllers/ProductController.cs
@@ -40,40 +40,50 @@ namespace DAWebERP1.Controllers
         // GET-Create
         public IActionResult Create()
         {
-            // TODO: ProductNames to view bag
-            var list = _db.ProductDivisions.Select(pd =>
-                                                        new SelectListItem
-                                                        {
-                                                            Value = pd.Id.ToString(),
-                                                            Text = pd.Name
-                                                        });
-            ViewBag.ProductNames = new SelectList(list, "Value", "Text");
-
+            CreateViewBagOfProductNames();
             return View();
         }
+
+        private void CreateViewBagOfProductNames()
+        {
+            var list = _db.ProductDivisions.Select(pd =>
+                                            new SelectListItem
+                                            {
+                                                Value = pd.Id.ToString(),
+                                                Text = pd.Name
+                                            });
+            ViewBag.ProductNames = new SelectList(list, "Value", "Text");
+        }
+
         // POST-Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(ProductModel product)
         {
-            ProductDivisionModel selectedProductDivision = _db.ProductDivisions
-                .Include(pd => pd.ProductKind)
-                .Include(pd => pd.ProductMaterial)
-                .Where(pd => pd.Id == product.ProductDivision.Id)
-                .FirstOrDefault();
-            product.ProductDivision = selectedProductDivision;
-            CustomOperations.CreateAndAsignDesignationFor(product);
-            ProductColorDesignModel productColorDesign = new ProductColorDesignModel();
-            ProductStrapModel productStrap = new ProductStrapModel();
-            product.ProductColorDesign = productColorDesign;
-            product.ProductStrap = productStrap;
-            product.DateCreated = System.DateTime.Today;
-            product.DateLastModified = System.DateTime.Today;
-            _db.Add(productColorDesign);
-            _db.Add(productStrap);
-            _db.Add(product);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                ProductDivisionModel selectedProductDivision = _db.ProductDivisions
+                        .Include(pd => pd.ProductKind)
+                        .Include(pd => pd.ProductMaterial)
+                        .Where(pd => pd.Id == product.ProductDivision.Id)
+                        .FirstOrDefault();
+                product.ProductDivision = selectedProductDivision;
+                CustomOperations.CreateAndAsignDesignationFor(product);
+                ProductColorDesignModel productColorDesign = new ProductColorDesignModel();
+                ProductStrapModel productStrap = new ProductStrapModel();
+                product.ProductColorDesign = productColorDesign;
+                product.ProductStrap = productStrap;
+                product.DateCreated = System.DateTime.Today;
+                product.DateLastModified = System.DateTime.Today;
+                _db.Add(productColorDesign);
+                _db.Add(productStrap);
+                _db.Add(product);
+                _db.SaveChanges();
+                return RedirectToAction("Index"); 
+            }
+            CreateViewBagOfProductNames();
+            return View(product);
         }
+
     }
 }

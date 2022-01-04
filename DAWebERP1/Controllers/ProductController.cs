457c1c51e238328ad422959db3040dd99446a3a3
miravaculka <miravaculka@gmail.com>
2022-01-04 14:17:41 +0100

    Delete and Update Product

--- DAWebERP1/Controllers/ProductController.cs
+++ DAWebERP1/Controllers/ProductController.cs
@@ -60,8 +60,8 @@ namespace DAWebERP1.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(ProductModel product)
         {
-            if (ModelState.IsValid)
-            {
+            //if (ModelState.IsValid)
+            //{
                 ProductDivisionModel selectedProductDivision = _db.ProductDivisions
                         .Include(pd => pd.ProductKind)
                         .Include(pd => pd.ProductMaterial)
@@ -80,10 +80,97 @@ namespace DAWebERP1.Controllers
                 _db.Add(product);
                 _db.SaveChanges();
                 return RedirectToAction("Index"); 
+            //}
+            //CreateViewBagOfProductNames();
+            //return View(product);
+        }
+        // Get-Delete
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            var product = _db.Products.Where(p => p.Id == Id).Include(p => p.ProductDivision).FirstOrDefault();
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        // POST-Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? Id)
+        {
+            var product = _db.Products.Where(p => p.Id == Id)
+                    .Include(product => product.ProductStrap)
+                    .Include(product => product.ProductColorDesign)
+                    .Include(product => product.ProductDivision)
+                        .ThenInclude(pd => pd.ProductKind)
+                    .Include(product => product.ProductDivision)
+                        .ThenInclude(pd => pd.ProductMaterial)
+                    .FirstOrDefault();
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _db.ProductColorDesigns.Remove(product.ProductColorDesign);
+                _db.ProductStraps.Remove(product.ProductStrap);
+                _db.Products.Remove(product);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        // GET-Update
+        public IActionResult Update(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            var product = _db.Products.Where(p => p.Id == Id).Include(p => p.ProductDivision).FirstOrDefault();
+            if (product == null)
+            {
+                return NotFound();
             }
             CreateViewBagOfProductNames();
             return View(product);
         }
-
+        // POST-Update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(ProductModel updatedProduct)
+        {
+            //if (ModelState.IsValid)
+            //{
+            var oldProduct = _db.Products.AsNoTracking().Where(p => p.Id == updatedProduct.Id)
+               .Include(product => product.ProductStrap)
+               .Include(product => product.ProductColorDesign)
+               .Include(product => product.ProductDivision)
+                   .ThenInclude(pd => pd.ProductKind)
+               .Include(product => product.ProductDivision)
+                   .ThenInclude(pd => pd.ProductMaterial)
+                .FirstOrDefault();
+            var updatedProductDivision = _db.ProductDivisions.AsNoTracking().Where(pd => pd.Id == updatedProduct.ProductDivision.Id)
+                .Include(pd => pd.ProductKind)
+                .Include(pd => pd.ProductMaterial)
+                .FirstOrDefault();
+            updatedProduct.ProductDivision = updatedProductDivision;
+            updatedProduct.DateCreated = oldProduct.DateCreated;
+            CustomOperations.CreateAndAsignDesignationFor(updatedProduct);
+            updatedProduct.DateLastModified = System.DateTime.Today;
+            _db.Update(updatedProduct.ProductColorDesign);
+            _db.Update(updatedProduct.ProductStrap);
+            _db.Products.Update(updatedProduct);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            //}
+            //CreateViewBagOfProductNames();
+            //return View(product);
+        }
     }
 }

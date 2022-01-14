3f36c9e29310eee790afc2a9438f20fd098c3d76
miravaculka <miravaculka@gmail.com>
2022-01-14 12:38:09 +0100

    Stock Controller and View created

---
+++ DAERP.Web/Controllers/StockController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DAERP.Web.Controllers
+{
+    public class StockController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

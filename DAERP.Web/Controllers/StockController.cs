dbb43f3b8c933c39e004d8ff5007b808cd2714f3
miravaculka <miravaculka@gmail.com>
2022-01-20 12:37:59 +0100

    Multi-select drop down implemented, read of stock page created

--- DAERP.Web/Controllers/StockController.cs
+++ DAERP.Web/Controllers/StockController.cs
@@ -1,4 +1,6 @@
-﻿using DAERP.Web.ViewModels;
+﻿using DAERP.DAL.DataAccess;
+using DAERP.Web.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
@@ -6,26 +8,42 @@ using System.Linq;
 
 namespace DAERP.Web.Controllers
 {
+    [Authorize]
     public class StockController : Controller
     {
+        private readonly ICustomerData _customerData;
+        private readonly ICustomerProductData _customerProductData;
+        public StockController(ICustomerData customerData, ICustomerProductData customerProductData)
+        {
+            _customerData = customerData;
+            _customerProductData = customerProductData;
+        }
+        [Authorize(Roles = "Admin,Manager,Cashier")]
         public IActionResult Index()
         {
-            var data = new List<MultiDropDownListViewModel> {
-            new MultiDropDownListViewModel { Id=1, Name="Bill"},
-            new MultiDropDownListViewModel { Id=2, Name="John"},
-            new MultiDropDownListViewModel { Id=3, Name="Doug"},
-            new MultiDropDownListViewModel { Id=4, Name="Sugan" },
-            new MultiDropDownListViewModel { Id=5, Name="Selina"},
-            new MultiDropDownListViewModel { Id=6, Name="Sylvia"},
-            new MultiDropDownListViewModel { Id=7, Name="Ubina" }
-            };
+            var activeCustomers = _customerData.GetAllCustomers().Where(c => c.State == "A");
+            var data = new List<MultiDropDownListViewModel>();
+            foreach (var customer in activeCustomers)
+            {
+                data.Add(new MultiDropDownListViewModel
+                {
+                    Id = customer.Id,
+                    Name = customer.Designation + ": " + customer.Name
+                });
+            }
             MultiDropDownListViewModel model = new();
             model.ItemList = data.Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString() }).ToList();
             return View(model);
         }
-
+        [Authorize(Roles = "Admin,Manager,Cashier")]
         [HttpPost]
-        public IActionResult PostSelectedValues(PostSelectedViewModel model)
+        public IActionResult PostSelectedCustomers(PostSelectedViewModel model)
+        {
+            
+            return RedirectToAction("Read");
+        }
+        [Authorize(Roles = "Admin,Manager,Cashier")]
+        public IActionResult Read()
         {
             return View();
         }

3057c89e51d93e8d987d7c534abeab9ff210b149
miravaculka <miravaculka@gmail.com>
2022-01-21 11:01:43 +0100

    Popover with product now works everywhere (js in site.js), Stock read view with working filtering and sorting

--- DAERP.Web/Controllers/StockController.cs
+++ DAERP.Web/Controllers/StockController.cs
@@ -1,11 +1,13 @@
 ﻿using DAERP.BL.Models;
 using DAERP.DAL.DataAccess;
+using DAERP.DAL.Services;
 using DAERP.Web.Helper;
 using DAERP.Web.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Routing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -42,17 +44,20 @@ namespace DAERP.Web.Controllers
         [HttpPost]
         public IActionResult PostSelectedCustomers(PostSelectedViewModel model)
         {
+            string customerIds = string.Join(";", model.SelectedIds);
             return RedirectToAction("Read", new RouteValueDictionary(
-                new { controller = "Stock", action = "Read", customersIds = model.SelectedIds }));
+                new { controller = "Stock", action = "Read", customersIds = customerIds }));
         }
         [Authorize(Roles = "Admin,Manager,Cashier")]
-        public IActionResult Read(int[] customersIds,
+        public IActionResult Read(string customersIds,
             string currentSort,
             string sortOrder,
             string currentFilter,
             string searchString,
             int? pageNumber)
         {
+            ViewData["CustomersIds"] = customersIds;
+            int[] customersIdsInt = customersIds.Split(";").ToList().Select(int.Parse).ToList().ToArray();
             if (sortOrder is null)
             {
                 sortOrder = currentSort;
@@ -67,8 +72,45 @@ namespace DAERP.Web.Controllers
             {
                 searchString = currentFilter;
             }
-            IEnumerable<CustomerProductModel> customerProducts = _customerProductData.GetProductsInStockOfCustomersWithChildModelsIncludedBy(customersIds);
+            IEnumerable<CustomerProductModel> customerProducts = _customerProductData.GetProductsInStockOfCustomersWithChildModelsIncludedBy(customersIdsInt);
             List<ProductCustomersReadViewModel> productCustomersViewModel = Mapper.ToProductCustomerReadViewModelListFrom(customerProducts.ToList());
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                string normalizedSearchString = searchString.Normalize(System.Text.NormalizationForm.FormD).ToUpper();
+                productCustomersViewModel = productCustomersViewModel.Where(pc =>
+                    pc.Product.Designation.Normalize(System.Text.NormalizationForm.FormD).ToUpper().Contains(normalizedSearchString) ||
+                    pc.Product.EAN.ToString().Contains(normalizedSearchString) ||
+                    pc.Product.ProductDivision.Name.Normalize(System.Text.NormalizationForm.FormD).ToUpper().Contains(normalizedSearchString) ||
+                    pc.Product.ProductDivision.ProductType.Normalize(System.Text.NormalizationForm.FormD).ToUpper().Contains(normalizedSearchString) ||
+                    pc.AmountInSelectedCustomersStocks.ToString().Contains(normalizedSearchString)
+                ).ToList();
+            }
+            ViewData["StockAmountSum"] = productCustomersViewModel.Select(vm => vm.AmountInSelectedCustomersStocks).Sum();
+            if (productCustomersViewModel.Count > 0)
+            {
+                string defaultPropToSort = "Product.Designation";
+                Helper.Helper.SetDataForSortingPurposes(ViewData, sortOrder, productCustomersViewModel.FirstOrDefault(), defaultPropToSort);
+                if (String.IsNullOrEmpty(sortOrder))
+                {
+                    sortOrder = defaultPropToSort;
+                }
+                bool descending = false;
+                if (sortOrder.EndsWith("_desc"))
+                {
+                    sortOrder = sortOrder.Substring(0, sortOrder.Length - 5);
+                    descending = true;
+                }
+                if (descending)
+                {
+                    productCustomersViewModel = productCustomersViewModel.OrderByDescending(e => DataOperations.GetPropertyValue(e, sortOrder))
+                        .ToList();
+                }
+                else
+                {
+                    productCustomersViewModel = productCustomersViewModel.OrderBy(e => DataOperations.GetPropertyValue(e, sortOrder))
+                        .ToList();
+                }
+            }
             int pageSize = 12;
             return View(PaginatedList<ProductCustomersReadViewModel>.Create(productCustomersViewModel, pageNumber ?? 1, pageSize));
         }
--- DAERP.Web/ViewModels/ProductCustomersReadViewModel.cs
+++ DAERP.Web/ViewModels/ProductCustomersReadViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using DAERP.BL.Models.Product;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DAERP.Web.ViewModels
 {
@@ -7,6 +8,7 @@ namespace DAERP.Web.ViewModels
     {
         public ProductModel Product { get; set; }
         public List<int> CustomerStockAmounts { get; set; }
+        [Display(Name = "Množství ve skladu všech odběratelů")]
         public int AmountInSelectedCustomersStocks { get; set; }
     }
 }
